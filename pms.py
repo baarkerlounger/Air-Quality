08d88a053c66038d1b39f2e264ba1574cb5283bd
Daniel Baark <db@slothlife.xyz>
2021-03-08 12:43:38 +0000

    Add PM dashboard

--- pms.py
+++ pms.py
@@ -11,22 +11,21 @@ client.switch_database('air_quality')
 # Configure the PMS5003
 pms5003 = PMS5003(
     device='/dev/ttyAMA0',
-    baudrate=9600,
+    baudrate=9600
 )
 
-try:
-    while True:
-        data = pms5003.read()
-        json_body = [
-            {
-                "measurement": "particulates",
-                "tags": {},
-                "fields": {
-                    "pm1.0": data.pm_ug_per_m3(1.0),
-                    "pm2.5": data.pm_ug_per_m3(2.5),
-                    "pm10": data.pm_ug_per_m3(10)
-                }
+while True:
+    data = pms5003.read()
+    json_body = [
+        {
+            "measurement": "particulates",
+            "tags": {},
+            "fields": {
+                "pm1.0": data.pm_ug_per_m3(1.0),
+                "pm2.5": data.pm_ug_per_m3(2.5),
+                "pm10": data.pm_ug_per_m3(10)
             }
-        ]
-        client.write_points(json_body)
-        time.sleep(5)
+        }
+    ]
+    client.write_points(json_body)
+    time.sleep(5)
